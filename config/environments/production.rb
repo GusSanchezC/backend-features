fce16bff0f3b906ea1efe14d8c14531eb18b3c28
GusSanchezC <galejandroscabrera@gmail.com>
2024-04-11 02:05:09 -0400

    Prueba heroku 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 require "active_support/core_ext/integer/time"
-Rails.application.routes.default_url_options = { host: "https://backend-features-63bad564e0c5.herokuapp.com" }
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_10_180411) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
-    t.integer "feature_id", null: false
+    t.bigint "feature_id", null: false
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
